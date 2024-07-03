f60dadaeb8cfb5eba5a012c1837777d6cafb986a
ex <orlovichlesha1@gmail.com>
2024-07-03 17:28:08 +0300

    adding a main menu, promotion field

---
+++ src/app/home-page/home-page/promotion-field/promotion-field.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-promotion-field',
+  templateUrl: './promotion-field.component.html',
+  styleUrl: './promotion-field.component.scss'
+})
+export class PromotionFieldComponent {
+
+}
