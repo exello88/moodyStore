5b048f4dd4dcb36a00cffaf4c0af2369a7d4d379
ex <orlovichlesha1@gmail.com>
2024-09-07 17:08:25 +0300

    add search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { LocalStorageService } from './local-storage.service';
+import { BehaviorSubject } from 'rxjs';
 import { AuthenticationService } from './authentication/authentication.service';
 
 @Component({
@@ -12,10 +13,15 @@ export class AppComponent implements OnInit {
   public cartItemCount: number = 0;
   public wishlistItemCount: number = 0;
   public email!: string;
-  public admin: boolean = false;
-  public auth: boolean = false;
+  public searchStr: string = '';
+  public searchStatus: boolean = false;
 
-  constructor(private router: Router, private localStorageService: LocalStorageService, private authServise: AuthenticationService) { }
+  private dataSubject: BehaviorSubject<string> = new BehaviorSubject<string>('');
+  searchDataSubject = this.dataSubject.asObservable();
+
+
+
+  constructor(private router: Router, private localStorageService: LocalStorageService, private authServise: AuthenticationService, private route: Router) { }
 
   ngOnInit() {
     this.changeCartItemCount();
@@ -28,14 +34,20 @@ export class AppComponent implements OnInit {
 
   public navigateToProfile(): void {
     this.router.navigate(['/admin']);
+    if (this.searchStatus)
+      this.searchStatus = false;
   }
 
   public navigateToCart(): void {
     this.router.navigate(['/cart']);
+    if (this.searchStatus)
+      this.searchStatus = false;
   }
 
   public navigateToWishlist(): void {
     this.router.navigate(['/wishlist']);
+    if (this.searchStatus)
+      this.searchStatus = false;
   }
 
   public changeCartItemCount(): void {
@@ -48,6 +60,18 @@ export class AppComponent implements OnInit {
     let wishlistItemCount = this.localStorageService.changeWishlistItemCount();
     if (wishlistItemCount !== null)
       this.wishlistItemCount = wishlistItemCount;
+  }
+  public test: boolean = false;
+
+  public changeSearchInput(): void {
+    if (this.searchStatus !== false)
+      this.dataSubject.next(this.searchStr);
+    if (this.searchStr.trim() === '') {
+      this.test = !this.test;
+      this.searchStatus = !this.searchStatus;
+      if (this.route.url !== '/home')
+        this.route.navigate(['home'])
+    }
   }
 
   public setEmail(): void {
